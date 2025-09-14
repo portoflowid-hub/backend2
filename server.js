5065c3597fffbb1357bd923875e48277bcf96364
Fariz Syahada <farizefanya@gmail.com>
2025-09-14 14:54:04 +0700

    ok

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ import adminRouter from './router/adminRoutes.js';
 import projectRouter from './router/projectRoutes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
+export const getUsers = async () => {
+  const res = await axios.get(`${API_URL}/api/users`, { withCredentials: true });
+  return res.data;
+};
+
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Backend API is running");
@@ -21,10 +30,14 @@ await connectedDB();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin: [
+      "http://localhost:3000",              // untuk dev lokal
+      "https://backend2-production-923e.up.railway.app/",  // domain vercel
+    ],
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
